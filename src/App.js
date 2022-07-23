f478258d51a2cf7afc90a2d4f298404a941d1625
0skars Rimša <afroamerikanis@gmail.com>
2022-07-24 02:01:55 +0300

    stash

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Obstacle from './components/Obstacle';
 
--- src/common/CommonObstacle.js
+++ src/common/CommonObstacle.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './CommonObstacle.css'
 
+const GetRandomCoordinates = () => {
+  let min = 1
+  let max = 99
+  let x = Math.floor(Math.random() * (max - min + 1)) + min
+  let y = Math.floor(Math.random() * (max - min + 1)) + min
+  // console.log("function " + x, y)
+  return {x, y}
+}
+
 function CommonObstacle() {
+  var style = {
+    top: GetRandomCoordinates().x,
+    left: GetRandomCoordinates().y
+  }
+  // const test = [GetRandomCoordinates()]
+  // console.log("one element " + test[0].x, test[0].y)
   return (
-    <div className="obstacle">TEST</div>
+    <div className="obstacle" style={style}>
+    </div>
   )
 }
 
